3f18e9a2ad06916b33b22446b7d0a40685dda47d
Kubo Ryosuke <sunfish.shogi@gmail.com>
2015-02-01 20:19:56 +0900

    Fix for gcc warnings

--- core/record/CsaReader.cpp
+++ core/record/CsaReader.cpp
@@ -135,8 +135,8 @@ namespace sunfish {
 	 * 持ち駒の読み込み
 	 */
 	bool CsaReader::_readHand(const char* line, Board& board, bool black) {
-		auto length = strlen(line);
-		for (auto i = 2; i + 4 <= length; i += 4) {
+		unsigned length = strlen(line);
+		for (unsigned i = 2; i + 4 <= length; i += 4) {
 			unsigned file = line[i+0] - '0';
 			unsigned rank = line[i+1] - '0';
 			Piece piece = Piece::parseCsa(&line[i+2]);
