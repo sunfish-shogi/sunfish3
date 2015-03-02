8d36b89f0edf6d68814e3443c534f25df9176346
Kubo Ryosuke <kubo_ryosuke@cyberagent.co.jp>
2015-03-03 02:25:10 +0900

    Fix SeeTest

--- searcher/test/SeeTest.cpp
+++ searcher/test/SeeTest.cpp
@@ -348,7 +348,7 @@ P-\n\
 
 		// 飛車で歩を取った場合
 		Move capByRook(Piece::Rook, P47, P45, false);
-		Value exact = see.search(eval, board, capByRook, -Value::Inf, Value::Inf);
+		Value exact = see.search(eval, board, capByRook, -Evaluator::PieceInf, Evaluator::PieceInf);
 		Value correct = (+ eval.table().pawnEx
 										 - eval.table().rookEx
 										 + eval.table().silverEx
@@ -357,7 +357,7 @@ P-\n\
 
 		// 桂馬で歩を取った場合
 		Move capByKnight(Piece::Knight, P37, P45, false);
-		exact = see.search(eval, board, capByKnight, -Value::Inf, Value::Inf);
+		exact = see.search(eval, board, capByKnight, -Evaluator::PieceInf, Evaluator::PieceInf);
 		correct = (+ eval.table().pawnEx
 							 - eval.table().knightEx);
 		ASSERT_EQ(correct.int32(), exact.int32());
@@ -383,7 +383,7 @@ P-\n\
 		CsaReader::readBoard(iss, board);
 
 		Move move(Piece::Lance, P45, P44, false);
-		Value exact = see.search(eval, board, move, -Value::Inf, Value::Inf);
+		Value exact = see.search(eval, board, move, -Evaluator::PieceInf, Evaluator::PieceInf);
 		Value correct = (+ eval.table().pawnEx
 										 - eval.table().lanceEx
 										 + eval.table().lanceEx
