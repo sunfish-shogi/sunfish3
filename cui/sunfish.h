425f81adb10de05692b4094d64b9015c1b146648
Kubo Ryosuke <sunfish.shogi@gmail.com>
2015-03-07 04:06:11 +0900

    Update copyright

--- cui/sunfish.h
+++ cui/sunfish.h
@@ -10,6 +10,6 @@
 
 #define SUNFISH_NAME			"Sunfish3"
 #define SUNFISH_VERSION			"1.0 alpha"
-#define SUNFISH_COPYRIGHT		"Copyright (c) 2014 Kubo Ryosuke"
+#define SUNFISH_COPYRIGHT		"Copyright (c) 2014-2015 Kubo Ryosuke"
 
 #endif /* SUNFISH_H_ */
