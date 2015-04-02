0e1965731101f6096f5abd128b6ca02bd43b0fd3
Kubo Ryosuke <sunfish.shogi@gmail.com>
2015-04-03 02:13:47 +0900

    Add all log types into network.log

--- cui/network.cpp
+++ cui/network.cpp
@@ -13,12 +13,15 @@ int network() {
 	// logger settings
 	std::ofstream fout("network.log", std::ios::out);
 	if (fout) {
+		Loggers::error.addStream(fout);
+		Loggers::warning.addStream(fout);
+		Loggers::message.addStream(fout);
 		Loggers::send.addStream(fout);
 		Loggers::receive.addStream(fout);
 	}
 	Loggers::error.addStream(std::cerr, "\x1b[31m", "\x1b[39m");
 	Loggers::warning.addStream(std::cerr, "\x1b[33m", "\x1b[39m");
-	Loggers::message.addStream(std::cerr/*, "\x1b[32m", "\x1b[39m"*/);
+	Loggers::message.addStream(std::cerr);
 	Loggers::send.addStream(std::cerr, true, true, "\x1b[34m", "\x1b[39m");
 	Loggers::receive.addStream(std::cerr, true, true, "\x1b[35m", "\x1b[39m");
 #ifndef NDEBUG
